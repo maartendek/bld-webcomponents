437a1ef593987cd21169541d98d99d8db3ee3df3
maartendek <dekker.maarten@gmail.com>
2022-02-21 14:56:37 +0100

    add react example

---
+++ bld-app-react/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bld-wc-stencil';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <my-awesome-counter count="9"></my-awesome-counter>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
